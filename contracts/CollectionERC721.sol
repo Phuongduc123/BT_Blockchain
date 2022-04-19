2e8731f4c9a415bfd8adbd25a730e2e5e6a151f6
Phuong duc123 <phuongceo13@gmail.com>
2022-04-19 23:59:17 +0700

    add contract collection 721

---
+++ contracts/CollectionERC721.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract CollectionERC721 is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+  function mint(string memory _tokenURI) public returns (uint256) {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mint(msg.sender, newItemId);
+    _setTokenURI(newItemId, _tokenURI);
+
+    return newItemId;
+  }
+}
---
+++ scripts/CollectionERC20.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  [address0] = await ethers.getSigners();
+  const CollectionERC721 = await hre.ethers.getContractFactory("CollectionERC721");
+  const collectionERC721 = await CollectionERC721.deploy("COLLECTION PHUONG", "PHUONG");
+
+  await collectionERC721.deployed();
+  collectionERC721.connect(address0).mint("https://ipfs.io/ipfs/QmWYqaxNntK9qU1k5wWYoqMiFV62aEyJEXWS3Bcpo2ohTq");
+
+
+  console.log("CollectionERC721 deployed to:", collectionERC721.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
